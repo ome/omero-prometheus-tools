1142c0f6e6a8f0696452fd730b1260cc1b6bdb02
Simon Li <spli@dundee.ac.uk>
2018-05-22 12:44:33 +0100

    Include prometheus-omero-sessions.service in python package

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ setup(
     name='omero-prometheus-tools',
     version='0.0.1',
     scripts=[
-        'sessions/omero-prometheus-sessions.py'
+        'sessions/omero-prometheus-sessions.py',
+    ],
+    data_files=[
+        ('service', ['sessions/prometheus-omero-sessions.service']),
     ],
     install_requires=[
-        'prometheus-client>=0.2,<0.3'
+        'prometheus-client>=0.2,<0.3',
     ],
+    # Allow external access to prometheus-omero-py-metrics.service file
+    zip_safe=False,
 )
