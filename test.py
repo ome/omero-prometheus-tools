7c182745678cce45c2e46bfe723dc7ec8dd1ce57
Simon Li <spli@dundee.ac.uk>
2018-09-20 14:00:44 +0100

    Switch to using omero-test-infra test-data and lib-test

--- test.py
+++ test.py
@@ -12,3 +12,5 @@ assert vals['omero_groups_total'] == '4.0'
 assert vals['omero_sessions_active{username="test-user"}'] == '1.0'
 assert (vals[u'omero_counts_projects_total{group="بيئة مجهرية مفتوحة"}'] ==
         '1.0')
+
+print('PASSED')
