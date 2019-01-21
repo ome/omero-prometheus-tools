777f502aac98811bd3e4a823794419afa012ed43
Simon Li <spli@dundee.ac.uk>
2019-01-21 15:03:48 +0000

    Bump version in setup.py 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='omero-prometheus-tools',
-    version='0.0.1',
+    version='0.1.2',
     scripts=[
         'omero_prometheus_tools/omero-prometheus-tools.py',
     ],
