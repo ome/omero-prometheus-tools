05becd9c182b6449e6870199e1a6ea451ca3100b
Sebastien Besson <seb.besson@gmail.com>
2019-02-05 09:46:58 +0000

    Bump to next development segment

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='omero-prometheus-tools',
-    version='0.1.3',
+    version='0.1.4.dev1',
     scripts=[
         'omero_prometheus_tools/omero-prometheus-tools.py',
     ],
